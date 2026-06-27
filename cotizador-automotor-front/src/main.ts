a89d1dcad46bdc4f53af932b8f87ef2979b6d5f9
her85 <herbotto1985@gmail.com>
2026-06-27 14:28:00 -0300

    Eliminar archivos no utilizados y reorganizar estilos en el proyecto

--- cotizador-automotor-front/src/main.ts
+++ cotizador-automotor-front/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import './style.css'
+import './assets/style.css'
 
 const router = createRouter({
   history: createWebHistory(),
